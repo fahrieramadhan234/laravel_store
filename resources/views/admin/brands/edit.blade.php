9b4f65bf1720503467b10eae0e7bb9b5f68c3359
fahrieramadhan234 <57474351+fahrieramadhan234@users.noreply.github.com>
2020-07-16 19:39:17 +0700

    update edit brand dan navbar

--- resources/views/admin/brands/edit.blade.php
+++ resources/views/admin/brands/edit.blade.php
@@ -5,31 +5,49 @@
 @endsection
 
 @section('content')
-<div class="content">
-    <div class="col-12">
-        <div class="card">
-            <form role="form" action="/admin/brand/update/{{$brand->brand_id}}" method="post"
-                enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <div class="modal-body">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="nim">Brand Name</label>
-                            <input name="brand_name" type="text" class="form-control" id="name" placeholder="Name"
-                                value="{{$brand->brand_name}}">
+<div class="content-page">
+    <div class="content">
+        <div class="content-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box">
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
+                                <li class="breadcrumb-item active">Update Product</li>
+                            </ol>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputFile">Logo</label>
-                            <input name="brand_logo" type="file" id="exampleInputFile">
-                        </div>
+                        <h4 class="page-title">Datatables</h4>
                     </div>
-                    <!-- /.box-body -->
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+            <div class="col-12">
+                <div class="card">
+                    <form role="form" action="/admin/brand/update/{{$brand->brand_id}}" method="post"
+                        enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <div class="modal-body">
+                            <div class="box-body">
+                                <div class="form-group">
+                                    <label for="nim">Brand Name</label>
+                                    <input name="brand_name" type="text" class="form-control" id="name"
+                                        placeholder="Name" value="{{$brand->brand_name}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">Logo</label>
+                                    <input name="brand_logo" type="file" id="exampleInputFile">
+                                </div>
+                            </div>
+                            <!-- /.box-body -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
